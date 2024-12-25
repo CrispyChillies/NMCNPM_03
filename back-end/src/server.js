fc35ae4c0047632876033145b8822e627db19ae6
kijen28 <phamquyen28112004@gmail.com>
2024-12-25 17:07:38 +0700

    Add something to be

---
+++ back-end/src/server.js
@@ -0,0 +1,51 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from './route/web';
+import {connectDB, queryDemo} from "./config/connectDB";
+import cors from 'cors';
+require('dotenv').config();
+
+let app = express();
+
+//config app
+app.use(cors()); // Enable CORS
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+viewEngine(app);
+initWebRoutes(app);
+
+let port = process.env.PORT || 6969;
+//Port === undefined => port = 6969
+
+
+app.get('/test-query', async (req, res) => {
+  try {
+    await queryDemo();
+    res.send('Query executed successfully. Check the console for results.');
+  } catch (err) {
+    res.status(500).send('Query failed.');
+  }
+});
+
+app.post('/signup', (req, res) => {
+  // Implement sign-up logic here
+  try{
+    res.send('Sign-up endpoint');
+  }catch(err){
+    res.status(500).send(err);
+  }
+});
+
+app.post('/signin', (req, res) => {
+  // Implement sign-in logic here
+  res.send('Sign-in endpoint');
+});
+
+app.listen(port, () => {
+    //callback
+    console.log("Backend Nodejs is runing on the port : " + port)
+})
+
+connectDB();
